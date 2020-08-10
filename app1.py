f710648a22964a86a615a748d5547851868c8eec
Pratik Garai <pratikgarai0208@gmail.com>
2020-08-11 02:27:20 +0530

    Added start stop methods

--- app1.py
+++ app1.py
@@ -7,5 +7,15 @@ class MainApp(App):
     def build(self):
         return Label(text="Hello World")
 
+    def on_start(self):
+        print("Starting...")
+        time.sleep(3)
+        print("Started!")
+
+    def on_stop(self):
+        print("Closing...")
+        time.sleep(3)
+        print("Bye!")
+
 if __name__=='__main__':
     MainApp().run()
