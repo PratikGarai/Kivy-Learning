08da10ec979de6c2d9d960c2df8c27aef3060c72
Pratik Garai <pratikgarai0208@gmail.com>
2020-08-11 02:26:35 +0530

    The basic app structure

---
+++ app1.py
@@ -0,0 +1,11 @@
+import time
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MainApp(App):
+    def build(self):
+        return Label(text="Hello World")
+
+if __name__=='__main__':
+    MainApp().run()
