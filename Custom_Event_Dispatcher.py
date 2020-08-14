17d05d104e5234d6412a99d69184d9c032b25b1e
PratikGarai <pratikgarai0208@gmail.com>
2020-08-14 16:33:22 +0530

    Corrected error in the Custome_Event, added driver code

--- Custom_Event_Dispatcher.py
+++ Custom_Event_Dispatcher.py
@@ -6,13 +6,23 @@ class MyEventDispacther(EventDispatcher):
 
     def __init__(self, **kwargs):
         # register the name of the event
-        self.register_event_type('callback')
+        # the name must begin with on_ 
+        self.register_event_type('on_callback')
         super(MyEventDispacther,self).__init__(**kwargs)
 
     def some_trigger(self, value):
         # calling this methos will trigger the event
         # with the given value
-        self.dispatch('callback', value)
+        self.dispatch('on_callback', value)
 
-    def callback(self, *args):
+    def on_callback(self, *args):
         print("The event was dispatched ",args)
+
+# Driver code
+
+def my_callback(valuse, *arge):
+    print("This is the sample callback driver")
+
+evd = MyEventDispacther()
+evd.bind(on_callback=my_callback)
+evd.some_trigger('Test value')
