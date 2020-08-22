55188269a040f2b4d24b91c091d86454a03ba765
PratikGarai <pratikgarai0208@gmail.com>
2020-08-23 01:32:13 +0530

    Page Layout made

--- Layouts/layout_page.py
+++ Layouts/layout_page.py
@@ -2,14 +2,16 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.pagelayout import PageLayout
 from kivy.config import Config
 
 Config.set('graphics','height',600)
 Config.set('graphics','width',600)
 
-Builder.load_file('layout_box.kv')
+Builder.load_file('layout_page.kv')
 
-class Layout(BoxLayout):
+class Layout(PageLayout):
     pass
 
 class MainApp(App):
